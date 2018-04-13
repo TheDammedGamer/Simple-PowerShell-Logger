d822faf44f9fb15f6c193f3a2b594bbbedf27231
Liam Townsend <the.dammed.gamer@gmail.com>
2018-04-13 15:44:32 +0100

    Added Comments to New-Logger.ps1
    Also added some logic to ensure it doesn't overwrite an existing file and it just adds to it.

--- Functions/New-Logger.ps1
+++ Functions/New-Logger.ps1
@@ -1,17 +1,26 @@
 
 class Logger {
+    #Where the Logger Will write to
     [string]$FilePath
+    #the Name used in the file
     [string]$Name
     [void]Log([string]$Text) {
-        $str = "[" + $(get-date -Format "yyyy.MM.dd-HH.mm.ss") + "]:[" + $this.Name + "] " + ": " + $Text
+        #Format: [2018.01.01-12.00.00]:[LogName] Text
+        $str = "[" + $(get-date -Format "yyyy.MM.dd-HH.mm.ss") + "]:[" + $this.Name + "] " + $Text
+        #Add to the Log File
         Add-Content -Path $this.FilePath -Value $str
+        #Write to the console
         Write-Host $str
     }
     Logger([string]$FilePath, [string]$Name) {
         $this.FilePath = $FilePath
         $this.Name = $Name
-        #Write-Host $FilePath
-        New-item -Path $this.FilePath -ItemType file -Force | out-null
+        #If the FilePath does not exist
+        if (!$(Test-Path $this.FilePath)) {
+            #Create the log file and don't return anything
+            New-item -Path $this.FilePath -ItemType file -Force | out-null
+        }
+        #Write to the File
         $this.Log("Starting new log")
     }
 }
@@ -23,11 +32,14 @@ function New-Logger {
     [Parameter(Position=1, Mandatory=$false)] [string]$FilePath
     )
     process {
+        #Check whether filepath has been passed through
         if (($FilePath -eq $null) -or ($FilePath -eq '')) {
+            #Format: C:\PSLogging\Log.LogName.01.01.18.log
             $FilePath = "C:\PSLogging\Log." + $Name + "." + $(Get-Date -Format 'dd.MM.yyy') + ".log"
         }
-        #Write-Host "FilePath: " $FilePath
+        # create a new instance of the logger class
         $Logger = [Logger]::new($FilePath, $Name)
+        #pass the logger back
         return $Logger
     }
 }
